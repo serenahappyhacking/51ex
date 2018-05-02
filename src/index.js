3c47a2c29e988453df5fabcb0d66ac22bb90bd7e
zhangzeming <zhangzeming@meituan.com>
2018-05-02 22:21:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,51 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index2.css';
+
+import logo from './img2/logo.svg';
 
 class Header extends React.Component {
-    render(){
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            isMenuOpen: false
+        }
+        this.toggleMenu = this.toggleMenu.bind(this)
+    }
+
+    toggleMenu() {
+        this.setState((prevState) => {
+            return {
+                isMenuOpen: !prevState.isMenuOpen
+            }
+        })
+    }
+
+    render() {
         return (
-            <div className="header">
-                <div className="navbar">
-                    <a className="navbar--logo">
-                        <svg width="67px" height="30px" viewBox="0 0 67 30" version="1.1" xmlns="http://www.w3.org/2000/svg" >
-                            <title>logo</title>
-                            <desc>Created with Sketch.</desc>
-                            <defs></defs>
-                            <g id="Page-1" stroke="none" strokeWidth="1" fill="none" >
-                                <g id="Home-page" transform="translate(-140.000000, -15.000000)" fill="#FFFFFF">
-                                    <g id="Navigation" transform="translate(120.000000, 0.000000)">
-                                        <g id="logo" transform="translate(20.000000, 15.000000)">
-                                            <polygon id="Fill-1" points="25.2953118 0 17.8517442 6.11031768 19.671097 8.17493674 24.9641944 3.67454456 24.9641944 29.2307692 27.9418605 29.2307692 27.9418605 0"></polygon>
-                                            <polygon id="Fill-2" points="62.0968238 0 54.0061742 11.8080099 45.8734159 0 42.1005867 0 52.0358449 13.8723306 41.1366279 29.2307692 44.9094571 29.2307692 53.9222595 16.0604688 62.9771708 29.2307692 66.75 29.2307692 55.850783 13.9135036 65.7021265 0"></polygon>
-                                            <polygon id="Fill-3" points="31.0465116 15.3846154 45.0174419 15.3846154 45.0174419 13.0769231 31.0465116 13.0769231"></polygon>
-                                            <polygon id="Fill-4" points="31.0465116 2.30769231 45.7936047 2.30769231 45.7936047 0 31.0465116 0"></polygon>
-                                            <polygon id="Fill-5" points="31.0465116 29.2307692 45.0174419 29.2307692 45.0174419 26.9230769 31.0465116 26.9230769"></polygon>
-                                            <path d="M17.91981,16.4885404 C17.447948,15.3591265 16.7818078,14.3882216 15.9213894,13.5752241 C15.0606669,12.7634296 14.047319,12.1366125 12.8816496,11.6953741 C11.7156762,11.2553388 10.4384261,11.0342684 9.05111542,11.0342684 C8.38467118,11.0342684 7.69724854,11.089912 6.99006364,11.1996952 C6.28196664,11.31008 5.5027255,11.510096 4.86455649,11.7302641 L4.86455649,2.70698402 L17.500546,2.64652804 L17.500546,0.00210543201 L2.12824346,0 L2.12824346,15.0388001 L2.13006767,15.0369954 L2.13006767,15.0839165 C3.07652795,14.5879369 4.07193782,14.1951234 5.11629729,13.9060777 C6.16035273,13.6170319 7.21140096,13.4723587 8.26974603,13.4723587 C9.32778707,13.4723587 10.3019145,13.6374847 11.1933445,13.9680375 C12.0844704,14.2985904 12.8570228,14.7675002 13.5116097,15.3729622 C14.1652845,15.9790259 14.6809275,16.7156263 15.0567144,17.5836659 C15.4328055,18.4511038 15.620699,19.4220088 15.620699,20.4963807 C15.620699,21.4880391 15.446487,22.4111207 15.0986712,23.2644222 C14.7502474,24.1192276 14.2698724,24.8555273 13.6578504,25.4754266 C13.0449163,26.0953259 12.3140167,26.5774699 11.4648475,26.9215576 C10.6150703,27.2665477 9.68989249,27.43799 8.68718579,27.43799 C7.37831606,27.43799 6.18801989,27.0806681 5.11629729,26.3639189 C4.04396662,25.6474705 3.24313901,24.6840849 2.71472656,23.4716569 L0,24.6696477 C0.807212343,26.295342 1.94855891,27.5898819 3.4249518,28.553869 C4.90012856,29.5178561 6.70974357,30 8.85440492,30 C10.3019145,30 11.6320667,29.7521606 12.8430372,29.2561809 C14.0543118,28.7602013 15.087726,28.0786429 15.9438879,27.2106034 C16.7991378,26.3431654 17.4604134,25.3103006 17.9277149,24.112009 C18.3944083,22.9137174 18.627907,21.6122596 18.627907,20.2070341 C18.627907,18.8580538 18.391672,17.6182551 17.91981,16.4885404" id="Fill-6"></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </a>
-                    <a href="#product" className="navbar--menu">产品介绍</a>
-                    <a href="#service" className="navbar--menu">服务</a>
-                    <a href="#aboutus" className="navbar--menu">关于我们</a>
-                </div>
-                <div className="hd-lan">
-				    <i className="selected icon-lan lan-zh"></i>
-                    <ul className="lan-list">
-                        <li data="zh">中文
-                            <i className="icon-lan lan-zh"></i>
-                        </li>
-                        <li data="en">English
-                            <i className="icon-lan lan-en"></i>
-                        </li>
-                    </ul>
-                </div>
+            <div className="header-wrapper">
+                <header className="header">
+                    <img src={logo} className="logo" />
+                </header>
+                <nav className={`menu ${this.state.isMenuOpen ? 'is-open' : ''}`}>
+                    <button className="menu-toggle" onClick={this.toggleMenu}>toggle menu</button>
+                    <div className="menu-dropdown">
+                        <ul className="nav-menu">
+                            <li><a href="#product">产品介绍</a></li>
+                            <li><a href="#service">服务</a></li>
+                            <li><a href="#aboutus">关于我们</a></li>
+                        </ul>
+                    </div>
+                </nav>
                 <div className="banner">
-                    <div className="banner--title">数字资产交易专业解决方案</div>
-                    <div className="banner--subtitle">轻松构建您自己的交易所</div>
+                    <div className="banner-title">数字资产交易专业解决方案</div>
+                    <div className="banner-subtitle">轻松构建您自己的交易所</div>
                 </div>
             </div>
         );
@@ -280,11 +275,11 @@ class App extends React.Component {
         return (
             <React.Fragment>
                 <Header />
-                <Product />
-                <Service />
-                <Advantage />
-                <AboutUs />
-                <Footer />
+                {/* <Product /> */}
+                {/* <Service /> */}
+                {/* <Advantage /> */}
+                {/* <AboutUs /> */}
+                {/* <Footer /> */}
             </React.Fragment>
         );
     }
